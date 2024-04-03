ee836bf0492988630360ca0a94ad301afaf102f2
Chris Hammock <chris@crshmk.com>
2024-04-03 14:35:33 +0700

    update packages

--- src/index.js
+++ src/index.js
@@ -3,6 +3,5 @@ import app from './app'
 const port = 1111
 
 app.listen(port, () => {
-  console.log('at', port)
-  console.log('db host is', process.env.DB_HOST)
+  console.log('service at', port)
 })
