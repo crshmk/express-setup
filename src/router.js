392197860c115cde2a014e0a6ebb0cc5585d9606
Chris Hammock <chris@crshmk.com>
2025-12-06 09:33:55 -0500

    add ping route

--- src/router.js
+++ src/router.js
@@ -4,6 +4,8 @@ import userRoutes from '@routes/userRoutes'
 
 const router = express.Router()
 
+router.get('/ping', (req, res) => res.send('pong'))
+
 router.use('/users', userRoutes)
 // router.use('/products', productRoutes)
 
